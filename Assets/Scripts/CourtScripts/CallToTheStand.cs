08b338008342e68d5e55e54c76a3d6af6de0e29b
BuildTools <unconfigured@null.spigotmc.org>
2016-11-18 04:22:15 -0500

    Court Testing Update

---
+++ Assets/Scripts/CourtScripts/CallToTheStand.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallToTheStand : MonoBehaviour {
+
+    public bool onStand = false;
+    public GameObject seatPosition;
+    private Transform standTarget;
+    private Transform seatTarget;
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start () {
+
+        GameObject standPosition = GameObject.FindGameObjectWithTag("StandPosition");
+
+        standTarget = standPosition.transform;
+        seatTarget = seatPosition.transform;
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (onStand == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, standTarget.position, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, seatTarget.position, moveSpeed * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/CourtScripts/CourtMenu.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CourtMenu : MonoBehaviour {
+    public GameObject accusedMenu;
+    public bool endScene;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.timeScale == 0 && Input.GetKeyUp(KeyCode.Space))
+        {
+            endScene = true;
+        }
+        if (Time.timeScale == 0 && Input.GetKeyDown(KeyCode.Space) && endScene == true)
+        {
+            Time.timeScale = 1;
+            accusedMenu.SetActive(false);
+            endScene = false;
+        }
+        if (Time.timeScale == 0 && Input.GetMouseButtonUp(0) && endScene == true)
+        {
+            Time.timeScale = 1;
+            accusedMenu.SetActive(false);
+            endScene = false;
+        }
+
+    }
+
+    void OnTriggerStay2D(Collider2D accusedCall)
+    {
+        if (accusedCall.gameObject.tag == "Player" && Time.timeScale == 1 && Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 0;
+            accusedMenu.SetActive(true);
+
+        }
+    }
+    public void AccusedChoice(string WitchChoice)
+    {
+        if (WitchChoice == "Choice_1")
+        {
+            endScene = true;
+        }
+        if (WitchChoice == "Choice_2")
+        {
+            endScene = true;
+        }
+        if (WitchChoice == "Choice_3")
+        {
+            endScene = true;
+        }
+        if (WitchChoice == "Choice_4")
+        {
+            endScene = true;
+        }
+        if (WitchChoice == "Choice_5")
+        {
+            endScene = true;
+        }
+        if (WitchChoice == "Choice_6")
+        {
+            endScene = true;
+        }
+        if (WitchChoice == "Choice_7")
+        {
+            endScene = true;
+        }
+        if (WitchChoice == "Choice_8")
+        {
+            endScene = true;
+        }
+        if (WitchChoice == "Choice_9")
+        {
+            endScene = true;
+        }
+        if (WitchChoice == "Choice_10")
+        {
+            endScene = true;
+        }
+
+    }
+
+}
